7e877805d4f3b29a835c2202a0403c790fd06213
ianbalijawa <ianbalijawa16@gmail.com>
2023-11-13 21:02:55 +0300

    feat: remove links in header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,27 +1,12 @@
 import {
 	createStyles,
-	Header as Wrapper, Group, UnstyledButton,
-	Text, ThemeIcon, Divider,
-	Center,
-	Box,
+	Header as Wrapper, Group, Box,
 	Burger,
-	Drawer,
-	Collapse,
-	ScrollArea,
-	rem,
+	Drawer, rem,
 	TextInput, Image
 } from '@mantine/core'
 import { useDisclosure } from '@mantine/hooks'
-import {
-	IconNotification,
-	IconCode,
-	IconBook,
-	IconChartPie3,
-	IconFingerprint,
-	IconCoin,
-	IconChevronDown,
-	IconSearch,
-} from '@tabler/icons-react'
+import { IconSearch } from '@tabler/icons-react'
 import SegmentedToggle from './ToggleColorScheme'
 import { Link } from 'react-router-dom'
 import { ROUTES } from '../constants/routes'
@@ -102,62 +87,10 @@ const useStyles = createStyles( theme => ( {
 	},
 } ) )
 
-const mockdata = [
-	{
-		icon: IconCode,
-		title: 'Open source',
-		description: 'This Pokémon’s cry is very loud and distracting',
-	},
-	{
-		icon: IconCoin,
-		title: 'Free for everyone',
-		description: 'The fluid of Smeargle’s tail secretions changes',
-	},
-	{
-		icon: IconBook,
-		title: 'Documentation',
-		description: 'Yanma is capable of seeing 360 degrees without',
-	},
-	{
-		icon: IconFingerprint,
-		title: 'Security',
-		description: 'The shell’s rounded shape and the grooves on its.',
-	},
-	{
-		icon: IconChartPie3,
-		title: 'Analytics',
-		description: 'This Pokémon uses its flying ability to quickly chase',
-	},
-	{
-		icon: IconNotification,
-		title: 'Notifications',
-		description: 'Combusken battles with the intensely hot flames it spews',
-	},
-]
-
 export function Header() {
 	const [drawerOpened, { toggle: toggleDrawer, close: closeDrawer }] =
 		useDisclosure( false )
-	const [linksOpened, { toggle: toggleLinks }] = useDisclosure( false )
-	const { classes, theme } = useStyles()
-
-	const links = mockdata.map( item => (
-		<UnstyledButton className={classes.subLink} key={item.title}>
-			<Group noWrap align="flex-start">
-				<ThemeIcon size={34} variant="default" radius="md">
-					<item.icon size={rem( 22 )} color={theme.fn.primaryColor()} />
-				</ThemeIcon>
-				<div>
-					<Text size="sm" fw={500}>
-						{item.title}
-					</Text>
-					<Text size="xs" color="dimmed">
-						{item.description}
-					</Text>
-				</div>
-			</Group>
-		</UnstyledButton>
-	) )
+	const { classes } = useStyles()
 
 	return (
 		<Box>
@@ -193,43 +126,9 @@ export function Header() {
 				className={classes.hiddenDesktop}
 				zIndex={1000000}
 			>
-				<ScrollArea h={`calc(100vh - ${rem( 60 )})`} mx="-md">
-					<Divider
-						my="sm"
-						color={
-							theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'
-						}
-					/>
-
-					<Link to={ROUTES.HOME} className={classes.link}>
-						Home
-					</Link>
-					<UnstyledButton
-						className={classes.link}
-						onClick={toggleLinks}
-					>
-						<Center inline>
-							<Box component="span" mr={5}>
-								Features
-							</Box>
-							<IconChevronDown
-								size={16}
-								color={theme.fn.primaryColor()}
-							/>
-						</Center>
-					</UnstyledButton>
-					<Collapse in={linksOpened}>{links}</Collapse>
-					<Divider
-						my="sm"
-						color={
-							theme.colorScheme === 'dark' ? 'dark.5' : 'gray.1'
-						}
-					/>
-
-					<Group position="center" grow px="md">
-						<SegmentedToggle />
-					</Group>
-				</ScrollArea>
+				<Group position="center" grow px="md">
+					<SegmentedToggle />
+				</Group>
 			</Drawer>
 		</Box>
 	)
